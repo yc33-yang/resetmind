7f599e2f8b678c9e88696c999d21afdc4e9e3fb3
yc33-yang <42262512+yc33-yang@users.noreply.github.com>
2018-08-13 00:19:56 +0000

    Update node.h
    internal members and methods
     - data, links(prev, next)
     - a constructor taking more arguments

--- node.h
+++ node.h
@@ -1,9 +1,16 @@
-// Copyright 2018 <Author>
+// Copyright 2018 yc33-yang
 
 #ifndef NODE_H_
 #define NODE_H_
 
 class Node {
+ char data;
+ Node *prev, *next;
+
+ Node(char _data, Node *_prev, Node *_next) : data(_data), prev(_prev), next(_next) {
+  // nothing to do more
+ }
+
  public:
   explicit Node(char data);
   char GetData();
