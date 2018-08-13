898c5f05bfa16243b27e8e0e5760335624ca3634
yc33-yang <42262512+yc33-yang@users.noreply.github.com>
2018-08-13 09:33:23 +0900

    Update typing_machine.cc
    First implementation of typing_machine.

--- typing_machine.cc
+++ typing_machine.cc
@@ -1,35 +1,88 @@
-// Copyright 2018 <Author>
+// Copyright 2018 yc33-yang
 
 #include "typing_machine.h"
 
+const int MAX_CHAR_PER_LINE = 100;
+
 TypingMachine::TypingMachine() {
-  return;
+	const char SOL = '\0', EOL = '\n';
+	home = new Node(SOL);
+	end = home->InsertNextNode(EOL);
+	cursor = end;
+	countChar = 0;
 }
 
 void TypingMachine::HomeKey() {
-  return;
+	cursor = home->GetNextNode();
 }
 
 void TypingMachine::EndKey() {
-  return;
+	cursor = end;
 }
 
 void TypingMachine::LeftKey() {
-  return;
+	Node *prev = cursor->GetPreviousNode();
+	if (prev != home)
+	{
+		cursor = prev;
+	}
 }
 
 void TypingMachine::RightKey() {
-  return;
+	Node *next = cursor->GetNextNode();
+	if (next != nullptr)
+	{
+		cursor = next;
+	}
+}
+
+inline bool isValidChar(char c) {
+	return (' ' <= c) && (c <= '~');
 }
 
 bool TypingMachine::TypeKey(char key) {
-  return false;
+	if (! isValidChar(key)) {
+		return false;
+	}
+	if (countChar >= MAX_CHAR_PER_LINE)
+	{
+		return false;
+	}
+	Node *prev = cursor->InsertPreviousNode(key);
+	if (prev == nullptr)
+	{
+		return false;
+	}
+	countChar++;
+	return true;
 }
 
 bool TypingMachine::EraseKey() {
-  return false;
+	if (cursor->GetPreviousNode() == home)
+	{
+		return false;
+	}
+	bool ret = cursor->ErasePreviousNode();
+	if (ret)
+	{
+		countChar--;
+	}
+	return ret;
 }
 
 std::string TypingMachine::Print(char separator) {
-  return "";
+	std::string line;
+	Node *iter;
+	for (iter = home->GetNextNode(); iter; iter = iter->GetNextNode())
+	{
+		if (iter == cursor && separator != 0) {
+			line += separator;
+		}
+		if (iter == end)
+		{
+			break;
+		}
+		line += iter->GetData();
+	}
+	return line;
 }
